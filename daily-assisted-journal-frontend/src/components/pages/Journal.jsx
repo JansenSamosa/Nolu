2cc80e3c18af6817b7afc2cd7218f661860c844b
Jansen Samosa <jansensamosa@Mac.lan>
2025-08-09 13:01:42 -0700

    removed mock_data.json and .gitignore in this directory, and removed imports in relevent files

--- daily-assisted-journal-frontend/src/components/pages/Journal.jsx
+++ daily-assisted-journal-frontend/src/components/pages/Journal.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
-import { fetchEntries } from '../../api'
 import { useFetchEntriesWithCache } from '../../api_handles/entriesHandler'
 import ButtonGlass from '../ui/ButtonGlass'
 import Header from '../ui/Header'
